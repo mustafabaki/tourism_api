746d239f1c013f81c0d8a3e98a4338e2bc575dbe
Mustafa Baki <mustafabakikucukbenli@gmail.com>
2023-10-06 11:50:36 +0300

    fixed

--- pages/api/myendpoint.ts
+++ pages/api/myendpoint.ts
@@ -7,6 +7,6 @@ import { translate } from '@vitalets/google-translate-api';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { param1, param2 } = req.query;
 
-  const { text } = await translate(`Neden böyle yapıyorsun? ${param1}`, { to: 'fr' });
+  const { text } = await translate(`Neden böyle yapıyorsun? ${param1} ${param2}`, { to: 'fr' });
   res.status(200).json({ message: text });
 };

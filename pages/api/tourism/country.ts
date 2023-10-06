49e97710cf3ad8f4beb4f611bfc587182e67dbbc
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2023-10-06 23:12:10 +0300

    omitted redundant comments

--- pages/api/tourism/country.ts
+++ pages/api/tourism/country.ts
@@ -14,34 +14,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   var data = response.data;
 
   const cheerio = require('cheerio');
-/*
-  var $ = cheerio.load(data);
 
-  // Select the container with class "keyfacts"
-  const keyFactsContainer = $('.keyfacts');
-
-  // Initialize an empty object to store the extracted information
-  const keyFacts: Record<string, string> = {};
-
-  // Select all rows with class "row info" within the container
-  const rows = keyFactsContainer.find('.row.info');
-
-  // Loop through each row and extract the key-value pairs
-  rows.each(async (_index: any, rowElement: any) => {
-    const row = $(rowElement);
-    const keyElement = row.find('.col-md-4');
-    const valueElement = row.find('.col-md-8 p');
-
-    // Extract and format the key
-    const key = keyElement.text().trim().replace(/:/g, '').replace(/\s+/g, '_').toLowerCase();
-    const value = valueElement.text().trim().replace(/./g, '');
-
-    // Translate the value using Google Translate API
-    const translatedValue = await translate(value, { to: language }); // Replace 'your_target_language' with the desired target language code
-
-    keyFacts[key] = translatedValue.text;
-  });
-*/
   // now, extract the parts you want using cheerio ... 
 
 
